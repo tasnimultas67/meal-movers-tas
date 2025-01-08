60968173b56534b4ef1f86cbd02d07b7550a72ec
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-08 18:19:11 +0600

    Update Hero component layout and styling for improved visuals and user engagement

--- components/HomeComponents/Hero.js
+++ components/HomeComponents/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1 className="text-6xl font-bold">
           Savor Every Bite, Delivered Right
         </h1>
-        <p className="w-8/12 text-sm text-gray-700">
+        <p className="w-10/12 text-sm text-gray-700">
           Whether you're in the mood for comfort food, healthy options, or
           gourmet dishes, we guarantee a seamless and timely delivery, so you
           can enjoy every bite without any hassle.
@@ -18,9 +18,9 @@ const Hero = () => {
           <input
             type="text"
             placeholder="Find your address"
-            className="border border-black p-2 text-sm w-6/12 rounded-tl rounded-bl"
+            className="border border-themeColor p-2 text-sm w-8/12 rounded-tl rounded-bl"
           />
-          <button className="bg-black p-[0.68rem] rounded-tr rounded-br">
+          <button className="bg-themeColor p-[0.68rem] rounded-tr rounded-br">
             <MagnifyingGlassIcon className="size-4 text-white" />
           </button>
         </div>
@@ -28,20 +28,22 @@ const Hero = () => {
       {/* Hero Fectured Image */}
       <div className="flex items-center justify-center">
         <div className="relative top-0">
-          <div className="p-14 bg-themeColor/20 rounded-2xl">
+          <div className="p-14 bg-themeColor/15 rounded-2xl">
             <Image
-              className="size-[20rem] object-cover object-top rounded-2xl"
+              className="size-[20rem] object-cover object-top rounded-2xl shadow-lg"
               width={500}
               height={500}
               alt="Meal Movers Image"
               src="https://plus.unsplash.com/premium_photo-1683983983955-fb43ce4f0d9b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
           </div>
-          <div className="px-3 py-1 text-xs bg-themeColor text-white w-fit rounded-md absolute bottom-5 -left-14">
-            1000+ Restaurants
+          <div className="px-3 py-1 text-xs bg-themeColor text-white w-fit rounded-md absolute bottom-5 -left-12 text-center shadow-lg">
+            <h2 className="text-2xl font-bold">1000+</h2>
+            <p>Restaurants</p>
           </div>
-          <div className="px-3 py-1 text-xs bg-themeColor text-white w-fit rounded-md absolute top-5 -right-14">
-            1000+ Restaurants
+          <div className="px-3 py-1 text-xs bg-themeColor text-white w-fit rounded-md absolute top-5 -right-10 text-center shadow-lg">
+            <h2 className="text-2xl font-bold">500+</h2>
+            <p>Reviews</p>
           </div>
         </div>
       </div>
