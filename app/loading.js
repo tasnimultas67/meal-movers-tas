a075dd81088ee0bfb22fdb91327aba6f13226e9b
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-08 22:21:17 +0600

    Add loading component for improved user experience during data fetching

---
+++ app/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div>
+        <h2 className="text-2xl font-semibold">Loading...</h2>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
