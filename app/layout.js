86800407ed96cd9e1d1c1e4991584a0764f2a3cc
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-08 19:54:27 +0600

    Add Footer component and integrate it into the layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -17,8 +18,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
-        <Header></Header>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
