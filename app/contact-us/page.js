f5099c14fe5aa2ee9d22ce58aa81986d11393269
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-10 22:37:30 +0600

    Enhance Contact Us page with improved layout and additional contact information

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h2>Contact Us</h2>
+    <div className="h-screen flex items-center justify-center w-11/12 m-auto ">
+      <div className="text-center space-y-5">
+        <h2 className="text-5xl font-bold">Contact Us</h2>
+        <p className="text-sm w-full md:w-7/12 m-auto">
+          We'd love to hear from you! Whether you have questions, feedback, or
+          need assistance, feel free to reach out to us. You can contact us via
+          phone at +880012345678910 or email at info@mealmovers.vercel.app. Our
+          team is here to help!
+        </p>
+      </div>
     </div>
   );
 };
