1e0bb5f8305fa520666d0f90867cefc6e5f85029
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-13 00:58:13 +0600

    Add restaurant listing feature with dynamic data rendering

--- app/restaurants/page.js
+++ app/restaurants/page.js
@@ -1,11 +1,29 @@
+import RestaurantCard from "@/components/RestaurantCard";
 import React from "react";
 
-const page = () => {
+const Page = async () => {
+  const fs = require("fs").promises;
+
+  const file = await fs.readFile(
+    process.cwd() + "/lib/restaurants.json",
+    "utf8"
+  );
+  const restaurantsData = JSON.parse(file);
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h2>restaurants</h2>
+    <div className="w-11/12 m-auto py-28 pb-10">
+      <div>
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-3">
+          {restaurantsData.map((restData) => (
+            <RestaurantCard
+              key={restData.restTitle}
+              restData={restData}
+            ></RestaurantCard>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
