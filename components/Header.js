99eb4f9470ef46c1940187a2132fee68445abd88
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-19 13:55:28 +0600

    Adjust user avatar size in Header component for improved layout

--- components/Header.js
+++ components/Header.js
@@ -112,7 +112,7 @@ export default function Header() {
                 <img
                   src={user.photoURL}
                   alt={user.displayName}
-                  className="w-10 h-10 rounded-full"
+                  className="w-8 h-8 rounded-full"
                 />
               </MenuButton>
               <MenuItems
